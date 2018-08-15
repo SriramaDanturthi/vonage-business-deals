987f2d99a111ace1f00b51d076ad259a6dfba20a
Sriram Danturthi <Sriram Danturthi>
2018-08-15 07:19:56 -0400

    initial checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { BannerComponent } from './banner/banner.component';
+import { MoreinfoComponent } from './moreinfo/moreinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PricingComponent,
+    BannerComponent,
+    MoreinfoComponent
   ],
   imports: [
     BrowserModule
